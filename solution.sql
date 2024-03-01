52daa3347ca2bea51386e5485c2e6c65ddd0db73
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-01 16:33:58 +0100

    SELECT 6

--- solution.sql
+++ solution.sql
@@ -19,5 +19,12 @@ SELECT * FROM `courses` WHERE `year` = 1 AND `period` LIKE 'I semestre';
 SELECT * FROM `exams` WHERE `date` = '2020/06/20' AND HOUR(`hour`) >= 14;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
+
+SELECT * FROM `degrees` WHERE `name` LIKE '% Laurea Magistrale %'
+//
+SELECT * FROM `degrees` WHERE `level` = 'magistrale'
+
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
