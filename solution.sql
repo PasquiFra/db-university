7bb5218e6f5cd18551ce0a4e365003a1a88b5730
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-01 16:34:13 +0100

    SELECT 7

--- solution.sql
+++ solution.sql
@@ -26,5 +26,7 @@ SELECT * FROM `degrees` WHERE `level` = 'magistrale'
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
+SELECT COUNT(*) as `number_of_departments` FROM `departments`
+
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
