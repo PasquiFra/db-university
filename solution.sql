9599025d9a466f19f49dc59515860d7a89f141a4
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-01 16:23:53 +0100

    SELECT 4

--- solution.sql
+++ solution.sql
@@ -11,6 +11,9 @@ SELECT * FROM `courses` WHERE `cfu` >= 10
     SELECT * FROM `students` WHERE (DATEDIFF(CURRENT_DATE(), DATE(`date_of_birth`)) / 365) >= 30
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+
+SELECT * FROM `courses` WHERE `year` = 1 AND `period` LIKE 'I semestre';
+
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 -- 7. Da quanti dipartimenti è composta l'università? (12)
