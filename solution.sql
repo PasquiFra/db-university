c99f8e46853a1d8283c0bcbde340fec9b8b03347
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-04 16:35:22 +0100

    JOIN 3

--- solution.sql
+++ solution.sql
@@ -77,7 +77,16 @@ JOIN `departments` ON `departments`.`id`=`degrees`.`department_id`
 WHERE `departments`.`name`= 'Dipartimento di Neuroscienze'
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT T.id AS 'Teacher_ID', C.id AS 'Course_ID', C.name AS 'Course_Name' FROM `courses` AS C
+INNER JOIN `course_teacher` AS CT ON CT.course_id = C.id
+JOIN `teachers` AS T ON T.id = CT.teacher_id
+WHERE T.name = 'Fulvio' AND T.surname = 'Amato'
+
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
+
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
