dab132ac8e1645877c4b419d0263ab3746863afb
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-04 16:17:04 +0100

    JOIN 1-2

--- solution.sql
+++ solution.sql
@@ -56,3 +56,28 @@ GROUP BY `exam_id`
 
 SELECT `department_id`, COUNT(`department_id`) AS `numof_degrees` FROM `degrees`
 GROUP BY `department_id`
+
+--! JOIN
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT 
+`students`.`id` AS `ID_Studente`, 
+`students`.`name` AS `Nome_Studente`,
+`students`.`surname` AS `Cognome_Studente`, 
+`degrees`.`name` AS `Nome_corso`
+FROM `students`
+INNER JOIN `degrees` ON `students`.`degree_id`=`degrees`.`id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT * FROM `degrees`
+JOIN `departments` ON `departments`.`id`=`degrees`.`department_id`
+WHERE `departments`.`name`= 'Dipartimento di Neuroscienze'
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
